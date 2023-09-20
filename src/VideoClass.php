ab3f1e27326203157a1a8626b72851607be263a0
Hunor Zakarias <0158@htl.rennweg.at>
2023-09-20 13:23:52 +0200

    implemented interface Vieo in abstract class Video

--- src/VideoClass.php
+++ src/VideoClass.php
@@ -1,7 +1,8 @@
 <?php
+require 'VideoInterface.php';
 declare(strict_types=1);
 
-abstract class Video{
+abstract class Video implements Video{
     string $name;
     string $source;
     string $html;
