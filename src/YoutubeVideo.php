9c4f2f301f23ff9231b591484c0e66900357758c
Hunor Zakarias <0158@htl.rennweg.at>
2023-09-21 11:50:43 +0200

    formated class YoutubeVideo.php

--- src/YoutubeVideo.php
+++ src/YoutubeVideo.php
@@ -12,6 +12,7 @@ class YoutubeVideo extends VideoClass
         $this->source = $source;
         $this->name = $name;
     }
+
     function getHTML(): string
     {
         return '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/' . $this->source . '" title="' . $this->name . '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
