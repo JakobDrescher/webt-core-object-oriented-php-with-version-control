293f748fdb8aca0c6ae7d0988f52c60efac41e91
Hunor Zakarias <0158@htl.rennweg.at>
2023-09-21 11:59:07 +0200

    added PHPDoc to VideoInterface.php

--- src/VideoInterface.php
+++ src/VideoInterface.php
@@ -3,7 +3,18 @@
 declare(strict_types=1);
 interface VideoInterface
 {
+    /**
+     * @return HTML <iframe> tag for displaying the video
+     */
     public function getHTML(): string;
+
+    /**
+     * @return source uniqe identifier of the video
+     */
     public function getSource(): string;
+
+    /**
+     * @return name title of the video
+     */
     public function getName(): string;
 }
