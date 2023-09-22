e541f01d8f80a3ec2c9a7691e69b9a0a36fe1dc5
JakobDrescher <jakobdrescher6@gmail.com>
2023-09-22 07:37:46 +0200

    created VimeoVideo class

---
+++ src/VimeoVideo.php
@@ -0,0 +1,29 @@
+<?php
+
+class VimeoVideo extends VideoClass
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function getHTML(): string
+    {
+        // TODO: Implement getHTML() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSource(): string
+    {
+        // TODO: Implement getSource() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getName(): string
+    {
+        // TODO: Implement getName() method.
+    }
+}
