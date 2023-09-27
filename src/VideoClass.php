eefd8216096eae15c9c9162d5e852c10e38fd23f
Hunor Zakarias <0158@htl.rennweg.at>
2023-09-27 10:06:35 +0200

    defined __construct in abstract class Video  removed __construct in YoutubeVideo and VimeoVideo as it is never overwritten  same with getter for name and source

--- src/VideoClass.php
+++ src/VideoClass.php
@@ -8,4 +8,20 @@ abstract class VideoClass implements VideoInterface
 {
     protected string $name;
     protected string $source;
+
+    function __construct(string $source, string $name)
+    {
+        $this->source = $source;
+        $this->name = $name;
+    }
+
+    function getSource(): string
+    {
+        return $this->source;
+    }
+
+    function getName(): string
+    {
+        return $this->name;
+    }
 }
--- src/VimeoVideo.php
+++ src/VimeoVideo.php
@@ -5,25 +5,8 @@ require_once 'VideoClass.php';
 
 class VimeoVideo extends VideoClass
 {
-    function __construct(string $source, string $name)
-    {
-        $this->source = $source;
-        $this->name = $name;
-    }
-
     public function getHTML(): string
     {
         return '<iframe width="560" height="315" src="https://player.vimeo.com/video/' . $this->source . '" title="' . $this->name . '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
-    }
-
-
-    public function getSource(): string
-    {
-        return $this->source;
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
     }
 }
